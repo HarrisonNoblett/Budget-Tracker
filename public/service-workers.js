2a7b9a1fd11ddf33a9e3160faa34bf06939175b4
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-03-07 13:55:20 -0600

    adding the service worker to the public folder

---
+++ public/service-workers.js
@@ -0,0 +1,12 @@
+const FILES_TO_CACHE = [
+    "/",
+    "/styles.css",
+    "/index.html",
+    "/index.js",
+    "/icons/icon-192x192.png",
+    "/icons/icon-512x512.png"
+];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
