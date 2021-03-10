ed0c2416f57d907ee532248a6befd347f697f586
Harrison Noblett <HarrisonNoblett55@gmail.com>
2021-03-09 18:08:31 -0600

    adding to heroku

--- public/service-workers.js
+++ public/service-workers.js
@@ -1,10 +1,10 @@
 const FILES_TO_CACHE = [
-    "/index.html",
-    "/styles.css",
-    "/index.js",
-    "/db.js",
-    "/icons/icon-192x192.png",
-    "/icons/icon-512x512.png",
+    "./index.html",
+    "./styles.css",
+    "./index.js",
+    "./db.js",
+    "./icons/icon-192x192.png",
+    "./icons/icon-512x512.png",
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
 ];
 
